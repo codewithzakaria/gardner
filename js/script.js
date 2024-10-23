1b6f288d40ec9fd0f13185c640913b48db739e40
codewithzakaria <zakidridimi@gmail.com>
2024-10-23 19:37:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Simple script to confirm message submission
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Merci pour votre message ! Nous vous répondrons rapidement.');
+});
